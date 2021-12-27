89658e96baf01797883c20244de09eab3b700449
Saurish Srivastava <saurishs1215@gmail.com>
2021-12-26 19:24:28 -0600

    feat(tailwind): v2.0 -> v3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,56 +1,10 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-      colors: {
-        gray: {
-          100: "#FBFBFB",
-          200: "#EAEAEA",
-          300: "#DFDFDF",
-          400: "#999999",
-          500: "#7F7F7F",
-          600: "#666666",
-          700: "#4C4C4C",
-          800: "#333333",
-          900: "#191919",
-        },
-        rose: {
-          50: "#FFF1F2",
-          100: "#FFE4E6",
-          200: "#FECDD3",
-          300: "#FDA4AF",
-          400: "#FB7185",
-          500: "#F43F5E",
-          600: "#E11D48",
-          700: "#BE123C",
-          800: "#9F1239",
-          900: "#881337",
-        },
-        blue: {
-          100: "#E6F0FD",
-          200: "#CCE2FC",
-          300: "#99C5FA",
-          400: "#66A9F7",
-          500: "#338CF5",
-          600: "#0070F4",
-          700: "#0064DA",
-          800: "#0059C2",
-          900: "#004391",
-        },
-        teal: {
-          100: "#E6FFFA",
-          200: "#B2F5EA",
-          300: "#81E6D9",
-          400: "#4FD1C5",
-          500: "#3ABAB4",
-          600: "#319795",
-          700: "#2C7A7B",
-          800: "#285E61",
-          900: "#234E52",
-        },
-      },
       boxShadow: {
         xs: "0 0 0 1px rgba(0, 0, 0, 0.16)",
         sm: "0 1px 2px 0 rgba(0, 0, 0, 0.16)",
@@ -136,13 +90,6 @@ module.exports = {
         "-1": "-1",
       },
     },
-  },
-  variants: {
-    backgroundColor: ["responsive", "hover", "focus", "group-hover"],
-    textColor: ["responsive", "hover", "focus", "group-hover"],
-    translate: ["responsive", "hover", "focus", "group-hover"],
-    boxShadow: ["responsive", "hover", "focus", "focus-within"],
-    opacity: ["responsive", "hover", "focus", "group-hover"],
   },
   plugins: [require("@tailwindcss/forms")],
 };
