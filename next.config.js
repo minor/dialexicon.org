693a198ae591dc0154786d7f4defb4a87e636d4f
Saurish Srivastava <saurishs1215@gmail.com>
2022-04-18 20:41:26 -0700

    update(redirects): add podcasts

--- next.config.js
+++ next.config.js
@@ -7,13 +7,25 @@ module.exports = {
         permanent: true,
       },
       {
+        source: "/youtube",
+        destination: "https://www.youtube.com/channel/UC-tO4dhpa6ab8evhqVcdvKA",
+        permanent: true,
+      },
+      {
         source: "/contact",
         destination: "mailto:dialexiconjournal@gmail.com",
         permanent: true,
       },
       {
         source: "/podcast",
-        destination: "https://www.youtube.com/channel/UC-tO4dhpa6ab8evhqVcdvKA",
+        destination:
+          "https://open.spotify.com/show/6ab9iObuaUb0VDo4R9lLH4?si=fbf776330bc544bd",
+        permanent: true,
+      },
+      {
+        source: "/apple",
+        destination:
+          "https://podcasts.apple.com/us/podcast/dialexicon/id1618727236",
         permanent: true,
       },
     ];
