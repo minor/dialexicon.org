066e6266d69158c6e59112fec03fe80f44c869cf
Saurish Srivastava <saurishs1215@gmail.com>
2022-05-11 19:00:03 -0700

    update(umami): umami link

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ class MyDocument extends Document {
             async
             defer
             data-website-id="d3ea84e3-0932-4197-ad9f-6919de67a19e"
-            src="https://umami-production-e355.up.railway.app/umami.js"
+            src="https://umami.saurish.com/umami.js"
           ></script>
         </Head>
         <body>
