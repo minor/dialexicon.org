1c2a269c52ae2cf330f95445c89e5eaffbfeaf99
Saurish Srivastava <saurishs1215@gmail.com>
2022-06-20 00:34:40 -0700

    add(redirects): yt

--- next.config.js
+++ next.config.js
@@ -2,30 +2,37 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: "/discord",
-        destination: "https://discord.gg/J5RzXgQRWK",
+        source: '/discord',
+        destination: 'https://discord.gg/J5RzXgQRWK',
         permanent: true,
       },
       {
-        source: "/youtube",
-        destination: "https://www.youtube.com/channel/UC-tO4dhpa6ab8evhqVcdvKA",
-        permanent: true,
-      },
-      {
-        source: "/contact",
-        destination: "mailto:dialexiconjournal@gmail.com",
-        permanent: true,
-      },
-      {
-        source: "/podcast",
+        source: '/youtube',
         destination:
-          "https://open.spotify.com/show/6ab9iObuaUb0VDo4R9lLH4?si=fbf776330bc544bd",
+          'https://www.youtube.com/channel/UC-tO4dhpa6ab8evhqVcdvKA',
         permanent: true,
       },
       {
-        source: "/apple",
+        source: '/yt',
         destination:
-          "https://podcasts.apple.com/us/podcast/dialexicon/id1618727236",
+          'https://www.youtube.com/channel/UC-tO4dhpa6ab8evhqVcdvKA',
+        permanent: true,
+      },
+      {
+        source: '/contact',
+        destination: 'mailto:dialexiconjournal@gmail.com',
+        permanent: true,
+      },
+      {
+        source: '/podcast',
+        destination:
+          'https://open.spotify.com/show/6ab9iObuaUb0VDo4R9lLH4?si=fbf776330bc544bd',
+        permanent: true,
+      },
+      {
+        source: '/apple',
+        destination:
+          'https://podcasts.apple.com/us/podcast/dialexicon/id1618727236',
         permanent: true,
       },
     ];
