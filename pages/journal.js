7605ef66205483972ec45cac33769704b199b38b
Saurish Srivastava <saurishs1215@gmail.com>
2022-07-18 17:16:26 -0700

    release(journal): 2022

--- pages/journal.js
+++ pages/journal.js
@@ -1,7 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
+import Head from 'next/head';
+import Image from 'next/image';
 
-import Layout from "../components/Layout";
+import Layout from '../components/Layout';
 
 export default function Journal() {
   return (
@@ -28,28 +28,28 @@ export default function Journal() {
               <div className="text-center">
                 <div
                   style={{
-                    position: "relative",
-                    width: "100%",
+                    position: 'relative',
+                    width: '100%',
                     height: 0,
-                    paddingTop: "125.0000%",
-                    paddingBottom: "48px",
-                    boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)",
-                    marginTop: "1.6em",
-                    marginBottom: "1.6em",
-                    overflow: "hidden",
-                    borderRadius: "8px",
-                    willChange: "transform",
+                    paddingTop: '125.0000%',
+                    paddingBottom: '48px',
+                    boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)',
+                    marginTop: '1.6em',
+                    marginBottom: '1.6em',
+                    overflow: 'hidden',
+                    borderRadius: '8px',
+                    willChange: 'transform',
                   }}
                 >
                   <iframe
                     loading="lazy"
                     style={{
-                      position: "absolute",
-                      width: "100%",
-                      height: "100%",
+                      position: 'absolute',
+                      width: '100%',
+                      height: '100%',
                       top: 0,
                       left: 0,
-                      border: "none",
+                      border: 'none',
                       padding: 0,
                       margin: 0,
                     }}
@@ -59,14 +59,16 @@ export default function Journal() {
                   ></iframe>
                 </div>
                 <a
-                  href="/journal2020.pdf"
+                  href="/journal2021.pdf"
                   className="text-gray-700 underline hover:text-black"
                   target="_blank"
                   rel="noopener"
                 >
-                  Dialexicon Journal Fall 2020
-                </a>{" "}
-                <span className="text-gray-700">by Elizabeth Zhu</span>
+                  Dialexicon Journal Spring 2022
+                </a>{' '}
+                <span className="text-gray-700">
+                  by Elizabeth Zhu
+                </span>
               </div>
             </div>
           </div>
@@ -86,39 +88,47 @@ export default function Journal() {
                 <br />
                 <br /> */}
                 <span className="font-semibold">
-                  Announcing the Winner and Published Authors of the{" "}
+                  Announcing the Winner and Published Authors of the{' '}
                   <a
-                    href="/"
+                    href="/journal2022.pdf"
                     className="text-gray-700 underline hover:text-black"
                     target="_blank"
                     rel="noopener"
                   >
-                    Dialexicon Fall 2021 Journal
+                    Dialexicon Spring 2022 Journal
                   </a>
                   .
-                </span>{" "}
+                </span>{' '}
                 <br />
                 <br />
-                ​Congratulations to Nick Liu, who is our 2021 winner and
-                recipient of the $100 prize, with his paper titled "Imagine
-                Sisyphus Exploited: The Absurdity of Late-Stage Capitalism." ​
+                ​Congratulations to Nick Liu of Phillips Academy
+                Andover (USA), who is our 2022 winner and recipient of
+                the $100 prize, with his paper titled "Imagine
+                Sisyphus Exploited: The Absurdity of Late-Stage
+                Capitalism." ​
                 <br />
                 <br />
-                Published Authors - Featured in the Dialexicon Fall 2021
-                Journal:
+                Published Authors - Featured in the Dialexicon Spring
+                2022 Journal:
                 <ul className="mt-4 mb-4 ml-6 list-disc">
                   <li>
-                    Marianne Stamotiou - "Compulsory or Voluntary Vaccination:
+                    Marianna Stamatiou (Arsakeio Lykeum of Patras,
+                    Greece) — "Compulsory or Voluntary Vaccination:
                     The Djokovic Case"
                   </li>
                   <li>
-                    Lucy Fan - "The Rise of Relativism and the Death of Truth"
+                    Lucy Fan (Guildford High School, United Kingdom) —
+                    "The Rise of Relativism and the Death of Truth"
                   </li>
                   <li>
-                    Jerry Zhang - "The Ethics of Mandatory Vaccines: Why
-                    Governments Should Take the Plunge"
+                    Jerry Zhang (Collingwood School, Canada) — "The
+                    Ethics of Mandatory Vaccines: Why Governments
+                    Should Take the Plunge"
                   </li>
-                  <li>Gabi Casals - "The Role of Identity"</li>
+                  <li>
+                    Gabi Casals (Brentwood College School, Canada) —
+                    "The Role of Identity"
+                  </li>
                 </ul>
                 Honorable Mentions:
                 <ul className="mt-4 mb-4 ml-6 list-disc">
@@ -136,55 +146,60 @@ export default function Journal() {
             </h1>
             <div className="mx-auto">
               <p className="mb-4 text-xl text-gray-600">
-                The 2020 Fall{" "}
+                The 2021 Spring{' '}
                 <span className="text-transparent bg-gradient-to-r from-rose-400 to-indigo-500 bg-clip-text">
                   Dialexicon
-                </span>{" "}
-                Journal received 85 submissions from 12 countries: Canada,
-                Greece, US, Mexico, Hungary, UK, Indonesia, Slovenia, Turkey,
-                Thailand, Bosnia and Herzegovina, and Serbia.
+                </span>{' '}
+                Journal received 85 submissions from 12 countries:
+                Canada, Greece, US, Mexico, Hungary, UK, Indonesia,
+                Slovenia, Turkey, Thailand, Bosnia and Herzegovina,
+                and Serbia.
                 <br />
                 <br />
                 <span className="font-semibold">
-                  Announcing the Winner and Published Authors of the{" "}
+                  Announcing the Winner and Published Authors of the{' '}
                   <a
-                    href="/journal2020.pdf"
+                    href="/journal2021.pdf"
                     className="text-gray-700 underline hover:text-black"
                     target="_blank"
                     rel="noopener"
                   >
-                    Dialexicon Fall 2020 Journal
+                    Dialexicon Spring 2021 Journal
                   </a>
                   .
-                </span>{" "}
+                </span>{' '}
                 <br />
                 <br />
-                ​Congratulations to Julia Wright, who is our 2020 winner and
-                recipient of the $500 prize, with her paper titled "The Animal
-                Rights Movement in the Context of the COVID-19 Pandemic." ​
+                ​Congratulations to Julia Wright, who is our 2021
+                winner and recipient of the $500 prize, with her paper
+                titled "The Animal Rights Movement in the Context of
+                the COVID-19 Pandemic." ​
                 <br />
                 <br />
-                Published Authors - Featured in the Dialexicon Fall 2020
-                Journal:
+                Published Authors - Featured in the Dialexicon Spring
+                2021 Journal:
                 <ul className="mt-4 mb-4 ml-6 list-disc">
                   <li>
-                    Kiki Ajayi (England) - "Does Violence have a place in
-                    Politics?"
+                    Kiki Ajayi (England) - "Does Violence have a place
+                    in Politics?"
                   </li>
                   <li>
-                    Dimitrios Kouvaras (Greece) - "COVID-19: A Lesson of Freedom
-                    and Citizenship"
+                    Dimitrios Kouvaras (Greece) - "COVID-19: A Lesson
+                    of Freedom and Citizenship"
                   </li>
                   <li>
-                    Sophie Nadalini (Canada) - "A Call for Nonviolent Protest"
+                    Sophie Nadalini (Canada) - "A Call for Nonviolent
+                    Protest"
                   </li>
                   <li>
                     Daniel Xu (USA) - "The Rebel in Black: A Black
-                    Existentialist Approach to the George Floyd Protests"
+                    Existentialist Approach to the George Floyd
+                    Protests"
                   </li>
                   <li>
-                    Emma Miléna Mészáros (Hungary) - "How Viktor Orban's Hungary
-                    is heading towards dictatorship during COVID-19"
+                    Emma Miléna Mészáros (Hungary) - "How Viktor
+                    Orban's Hungary is heading towards dictatorship
+                    during COVID-19"
                   </li>
                 </ul>
                 Honorable Mentions:
@@ -195,6 +210,53 @@ export default function Journal() {
                   <li>Eva Kamimura (Canada)</li>
                 </ul>
               </p>
+            </div>
+            <div className="mx-auto">
+              <div className="text-center">
+                <div
+                  style={{
+                    position: 'relative',
+                    width: '100%',
+                    height: 0,
+                    paddingTop: '125.0000%',
+                    paddingBottom: '48px',
+                    boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)',
+                    marginTop: '1.6em',
+                    marginBottom: '1.6em',
+                    overflow: 'hidden',
+                    borderRadius: '8px',
+                    willChange: 'transform',
+                  }}
+                >
+                  <iframe
+                    loading="lazy"
+                    style={{
+                      position: 'absolute',
+                      width: '100%',
+                      height: '100%',
+                      top: 0,
+                      left: 0,
+                      border: 'none',
+                      padding: 0,
+                      margin: 0,
+                    }}
+                    src="https://www.canva.com/design/DAEOGgg2VpY/view?embed"
+                    allowFullScreen="allowfullscreen"
+                    allow="fullscreen"
+                  ></iframe>
+                </div>
+                <a
+                  href="/journal2021.pdf"
+                  className="text-gray-700 underline hover:text-black"
+                  target="_blank"
+                  rel="noopener"
+                >
+                  Dialexicon Journal Spring 2021
+                </a>{' '}
+                <span className="text-gray-700">
+                  by Elizabeth Zhu
+                </span>
+              </div>
             </div>
           </div>
         </div>
